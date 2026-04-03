d60f67586e4915689becedc049af1dfd1c597f91
kwakseobang <iii6602@naver.com>
2026-04-04 00:21:34 +0900

    Fix[#196]: 회원 학습 설정 조회 시 특정 날짜의 학습 정보 필터링 로직 개선

--- src/test/java/org/kwakmunsu/haruhana/domain/member/service/MemberManagerIntegrationTest.java
+++ src/test/java/org/kwakmunsu/haruhana/domain/member/service/MemberManagerIntegrationTest.java
@@ -120,7 +120,12 @@ class MemberManagerIntegrationTest extends IntegrationTestSupport {
         memberManager.updatePreference(memberPreference, updatePreference);
 
         // then
-        var newMemberPreference = memberReader.getMemberPreferences(member.getId()).getFirst();
+        entityManager.flush();
+        entityManager.clear();
+        var newMemberPreference = memberReader.getMemberPreferences(member.getId()).stream()
+                .filter(preference -> preference.getEffectiveAt().equals(LocalDate.now().plusDays(1)))
+                .findFirst()
+                .orElseThrow();
         assertThat(newMemberPreference).isNotNull()
                 .extracting(
                         MemberPreference::getCategoryTopic,
@@ -152,7 +157,12 @@ class MemberManagerIntegrationTest extends IntegrationTestSupport {
         memberManager.updatePreference(memberPreference, updatePreference);
 
         // 같은 날짜에 두번째 업데이트 준비
-        var newMemberPreference = memberReader.getMemberPreferences(member.getId()).getFirst();
+        entityManager.flush();
+        entityManager.clear();
+        var newMemberPreference = memberReader.getMemberPreferences(member.getId()).stream()
+                .filter(preference -> preference.getEffectiveAt().equals(LocalDate.now().plusDays(1)))
+                .findFirst()
+                .orElseThrow();
         var mysqlCategory = categoryTopicJpaRepository.findByName("MySQL")
                 .orElseThrow(() -> new RuntimeException("MySQL 토픽이 존재하지 않습니다"));
         var updateSecPreference = new UpdatePreference(newMemberPreference.getId(), mysqlCategory.getId(), ProblemDifficulty.EASY);
@@ -161,11 +171,13 @@ class MemberManagerIntegrationTest extends IntegrationTestSupport {
         memberManager.updatePreference(newMemberPreference, updateSecPreference);
 
         // then
+        entityManager.flush();
+        entityManager.clear();
         var sameIdPreference = memberReader.getMemberPreference(newMemberPreference.getId(), member.getId());
 
         // 동일한 엔티티가 업데이트 되었는지 확인
         assertThat(sameIdPreference.getId()).isEqualTo(newMemberPreference.getId());
-        assertThat(newMemberPreference).isNotNull()
+        assertThat(sameIdPreference).isNotNull()
                 .extracting(
                         MemberPreference::getCategoryTopic,
                         MemberPreference::getDifficulty,
